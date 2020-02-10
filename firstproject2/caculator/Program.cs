305a48ce1ac2730934d4854841aaa4590741e748
Jin <jinwoov@gmail.com>
2020-02-10 11:47:24 -0800

    added my project

---
+++ firstproject2/caculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using caculator.calc;
+
+namespace caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What operation do you want do? (+,-,*,/)");
+            string oper = Console.ReadLine();
+            if (oper == "+")
+            {
+                Console.WriteLine("write first number");
+                string num = Console.ReadLine();
+
+
+
+
+            }
+            
+        }
+    }
+}
---
+++ firstproject2/caculator/calc/operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caculator.calc
+{
+    class operation
+    {
+        public static int Add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 + Num2;
+            return Answer;
+        }
+        public static int Minus(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 - Num2;
+            return Answer;
+        }
+        public static int Divide(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1/Num2;
+            return Answer;
+        }
+        public static int Times(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 * Num2;
+            return Answer;
+        }
+    }
+}
