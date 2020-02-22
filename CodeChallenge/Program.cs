698280117c1f41f58e3dc9d5463cfb2001210bd2
Jin <jinwoov@gmail.com>
2020-02-22 04:09:57 -0800

    finished first code challenge

---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool userChoice = true;
+           while (userChoice)
+           userChoice = Mainmenu();
+           
+        }
+    
+        private static bool Mainmenu()
+        {
+           Console.WriteLine("Choose an option of application");
+           Console.WriteLine("1. Array Max Result");
+           Console.WriteLine("2. Leap Year Calculator");
+           Console.WriteLine("3. Perfect Sequence");
+           Console.WriteLine("4. EXIT");
+           int userChoice = int.Parse(Console.ReadLine());
+           if (userChoice == 1) {
+               ArrayMaxResult();
+               return true;
+           }
+           else {
+               return false;
+           }
+        }
+        private static void ArrayMaxResult()
+        {
+            int[] choiceArray = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+            Console.WriteLine($"Choose five numbers between 1-10({i+1}/5)");
+            choiceArray[i]= int.Parse(Console.ReadLine());
+            }
+            int myScore = 4;
+            int counter = 0;
+            foreach (var numbers in choiceArray)
+            {
+                if (numbers == myScore)
+                counter++;
+            }
+            myScore *=counter;
+            Console.WriteLine(
+                "Your choices were: {0}.", 
+                String.Join(", ", choiceArray));
+            Console.WriteLine(
+                "My choice was {0}, and overall you got {1}.",
+                4, myScore);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# C-CS
-Repo strictly for education purposes. 
+# .NET Prework
+MIT based repository that contains Code Fellows 401 pre-work.
 
 Author: Jin Kim
 
 Prework  
 ![dotnet prework version](dotnet-version.jpg)
+
+## Code-Challenge
+Problem 1: Array Max Result
+  
+### Instruction of Challenge
+Run the application by typing `dotnet run` in the /code-challenge folder. This will prompt main menu. type 1 and enter to open `Array Max Result` application.
+Thereafter, type in 5 inputs and follow instruction. Upon entering five numbers console will prompt my number and how many points user earned.
+
+![dotnet prework code challenge #1](array-max-result.jpg)
+
 
 
 
