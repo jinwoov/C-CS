111a64cc7ac174db5be5b4e56f5d9a3637d53918
Jin <jinwoov@gmail.com>
2020-02-24 20:44:10 -0800

    finished my challenge

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,15 +6,21 @@ namespace code_challenge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
             bool userChoice = true;
-            while (userChoice)
-                userChoice = Mainmenu();
+            while (userChoice) 
+            {
+                userChoice = Mainmenu(userName);
+            }
+            Console.WriteLine($"Thank you for playing bye~ {userName}");
+            Console.ReadLine();
 
         }
 
-        private static bool Mainmenu()
+        private static bool Mainmenu(string name)
         {
-            Console.WriteLine("Choose an option of application");
+            Console.WriteLine($"Hello {name}, choose an option of application");
             Console.WriteLine("1. Array Max Result");
             Console.WriteLine("2. Leap Year Calculator");
             Console.WriteLine("3. Perfect Sequence");
@@ -23,22 +29,22 @@ namespace code_challenge
             int userChoice = int.Parse(Console.ReadLine());
             if (userChoice == 1)
             {
-                ArrayMaxResult();
+                ArrayMaxResult(name);
                 return true;
             }
             else if (userChoice == 2)
             {
-                LeapYearCalc();
+                LeapYearCalc(name);
                 return true;
             }
             else if (userChoice == 3)
             {
-                PerfectSequence();
+                PerfectSequence(name);
                 return true;
             }
             else if (userChoice == 4)
             {
-                SumOfRows();
+                SumOfRows(name);
                 return true;
             }
             else
@@ -46,12 +52,12 @@ namespace code_challenge
                 return false;
             }
         }
-        private static void ArrayMaxResult()
+        private static void ArrayMaxResult(string name)
         {
             int[] choiceArray = new int[5];
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"Choose five numbers between 1-10({i + 1}/5)");
+                Console.WriteLine($"{name}, choose five numbers between 1-10({i + 1}/5)");
                 choiceArray[i] = int.Parse(Console.ReadLine());
             }
             int myScore = 4;
@@ -63,36 +69,34 @@ namespace code_challenge
             }
             myScore *= counter;
             Console.WriteLine(
-                "Your choices were: {0}.",
-                String.Join(", ", choiceArray));
+                "{1}, your choices were: {0}.",
+                String.Join(", ", choiceArray), name);
             Console.WriteLine(
                 "My choice was {0}, and overall you got {1}.",
                 4, myScore);
         }
-        private static void LeapYearCalc()
+        private static void LeapYearCalc(string name)
         {
             Console.WriteLine("Enter a year");
             int year = int.Parse(Console.ReadLine());
             if (year % 4 == 0 && year % 100 != 0)
             {
-                Console.WriteLine("it is leap year");
+                Console.WriteLine("Congratulation {0}, it is leap year", name);
             }
             else if (year % 4 == 0 && year % 400 == 0)
             {
-                Console.WriteLine("it is leap year");
+                Console.WriteLine("Congratulation {0}, it is leap year", name);
             }
             else
             {
-                Console.WriteLine("It is not leap year");
+                Console.WriteLine("Sorry {0}, It is not leap year", name);
             }
         }
 
-        private static void PerfectSequence()
+        private static void PerfectSequence(string name)
         {
-            Console.WriteLine("Type in array of numbers to check if its perfect sequence");
+            Console.WriteLine("Type in numbers of choice with comma inbetween each number of numbers to check if its perfect sequence");
             string userInput = Console.ReadLine();
-            userInput = userInput.Replace("[", "");
-            userInput = userInput.Replace("]", "");
             string[] newArray = userInput.Split(",");
             int[] numArray = Array.ConvertAll(
                 newArray, str => int.Parse(str));
@@ -101,8 +105,8 @@ namespace code_challenge
             foreach (var value in numArray)
             {
                 if (value<0) {
-                    Console.WriteLine("You entered negative number");
-                    Console.WriteLine("No");
+                    Console.WriteLine("{2}, you entered {0} and you've entered negative value of {1}", userInput, value, name);
+                    Console.WriteLine("No, it is not a perfect sqeuence.");
                     return;
                 }
                 else {
@@ -111,17 +115,19 @@ namespace code_challenge
                 }
             }
             if (count == countMult){
-                Console.WriteLine("Yes");
+                Console.WriteLine("{1}, you entered {0}",userInput, name);
+                Console.WriteLine("Yes, it is perfect sequence!");
             }
             else {
-                Console.WriteLine("No");
+            Console.WriteLine("{1}, you entered {0}",userInput, name);
+            Console.WriteLine("No, it is not a perfect sqeuence.");
             }
         }
-        private static void SumOfRows()
+        private static void SumOfRows(string name)
         {   
-            Console.WriteLine("Please type in length of the array");
+            Console.WriteLine($"{name}, please type in length of the array");
             int lengthNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please type in width of the array");
+            Console.WriteLine($"{name}, please type in width of the array");
             int widthNum = int.Parse(Console.ReadLine());
 
             Random randomNum = new Random();
@@ -139,7 +145,7 @@ namespace code_challenge
                 }
                 sumArray[i] = count;
             }
-            Console.WriteLine("[{0}]", String.Join(", ", sumArray));
+            Console.WriteLine("The sum of each row was, [{0}]", String.Join(", ", sumArray));
         }
     }
 }
