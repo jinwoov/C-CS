17fb5897a02a17cdf8bc708a8c84aa516fc1e01b
Jin <jinwoov@gmail.com>
2020-03-09 17:52:35 -0700

    added style to the console application

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,7 +6,15 @@ namespace code_challenge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"
+                                ================================
+                                #   Welcome to my application  #
+                                #      made by Jin             #
+                                ================================");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("What's your name?");
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.White;
             string userName = Console.ReadLine();
             bool userChoice = true;
             while (userChoice) 
@@ -14,19 +22,28 @@ namespace code_challenge
                 userChoice = Mainmenu(userName);
             }
             Console.WriteLine($"Thank you for playing bye~ {userName}");
-            Console.ReadLine();
+            Environment.Exit(0);
 
         }
 
         private static bool Mainmenu(string name)
         {
+            Console.WriteLine("");
             Console.WriteLine($"Hello {name}, choose an option of application");
             Console.WriteLine("1. Array Max Result");
             Console.WriteLine("2. Leap Year Calculator");
             Console.WriteLine("3. Perfect Sequence");
             Console.WriteLine("4. Sum of Rows");
             Console.WriteLine("5. EXIT");
-            int userChoice = int.Parse(Console.ReadLine());
+            int userChoice;
+            bool numberCheck = Int32.TryParse(Console.ReadLine(), out userChoice);
+            while(!numberCheck)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a number");
+                Console.ForegroundColor = ConsoleColor.White;
+                numberCheck = Int32.TryParse(Console.ReadLine(), out userChoice);
+            }
             if (userChoice == 1)
             {
                 ArrayMaxResult(name);
@@ -54,12 +71,22 @@ namespace code_challenge
         }
         private static void ArrayMaxResult(string name)
         {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
             int[] choiceArray = new int[5];
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"{name}, choose five numbers between 1-10({i + 1}/5)");
-                choiceArray[i] = int.Parse(Console.ReadLine());
+                bool checkNumb = Int32.TryParse(Console.ReadLine(), out choiceArray[i]);
+                while(!checkNumb)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("please type in number! -_-^");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    checkNumb = Int32.TryParse(Console.ReadLine(), out choiceArray[i]);
+                }
             }
+            Console.Beep();
             int myScore = 4;
             int counter = 0;
             foreach (var numbers in choiceArray)
@@ -68,34 +95,72 @@ namespace code_challenge
                     counter++;
             }
             myScore *= counter;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(
                 "{1}, your choices were: {0}.",
                 String.Join(", ", choiceArray), name);
-            Console.WriteLine(
-                "My choice was {0}, and overall you got {1}.",
-                4, myScore);
-        }
-        private static void LeapYearCalc(string name)
-        {
-            Console.WriteLine("Enter a year");
-            int year = int.Parse(Console.ReadLine());
-            if (year % 4 == 0 && year % 100 != 0)
+            if (myScore <= 8)
             {
-                Console.WriteLine("Congratulation {0}, it is leap year", name);
-            }
-            else if (year % 4 == 0 && year % 400 == 0)
-            {
-                Console.WriteLine("Congratulation {0}, it is leap year", name);
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(
+                "My choice was {0}, and overall you got {1} points.", 4, myScore);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else
             {
-                Console.WriteLine("Sorry {0}, It is not leap year", name);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine(
+                "********* Congratulation! My choice was {0}, and overall you got {1} points. ***********", 4, myScore);
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+        private static void LeapYearCalc(string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("");
+            Console.WriteLine("Enter a year to check for leap year");
+            Console.ForegroundColor = ConsoleColor.White;
+            int year; 
+            bool checkNumb = Int32.TryParse(Console.ReadLine(), out year);
+            while(!checkNumb)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a year in numerical value _-_;");
+                Console.ForegroundColor = ConsoleColor.White;
+                checkNumb = Int32.TryParse(Console.ReadLine(), out year);
+            }
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
+                Console.WriteLine("Yes! {0}, {1} is leap year", name, year);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else if (year % 4 == 0 && year % 400 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
+                Console.WriteLine("Yes! {0}, {1} is leap year", name, year);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.WriteLine("T.T Sorry {0}, {1} is not leap year", name, year);
             }
         }
 
         private static void PerfectSequence(string name)
         {
-            Console.WriteLine("Type in numbers of choice with comma inbetween each number of numbers to check if its perfect sequence");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("");
+            Console.WriteLine("Type in numbers of choice with comma in-between each number of numbers to check if its perfect sequence");
+            Console.ForegroundColor = ConsoleColor.White;
             string userInput = Console.ReadLine();
             string[] newArray = userInput.Split(",");
             int[] numArray = Array.ConvertAll(
--- Prework-CodeChallenges/menuoption/menu.cs
+++ Prework-CodeChallenges/menuoption/menu.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace code_challenge
 {
-    class mainMenu
+    class menu
     {
     }
 }
